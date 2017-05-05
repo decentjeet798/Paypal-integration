530bf4a1d5973b90247ba4db623505ca68fe1be0
vinaysurya <vinisuri@gmail.com>
2017-05-05 15:58:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vinay/paypal/PayPalConfig.java
@@ -0,0 +1,8 @@
+package com.example.vinay.paypal;
+
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AUDWqx2LwkhefmjMoM5WfrKzWFyl-c7-qZ5jIKePyR2c3wJK4eWqti9Uflu04nkmmfzQE2Yy9_R9oy7w";
+}
+
